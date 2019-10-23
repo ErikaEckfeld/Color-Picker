efbcd158175ca77a9fd9ec2f5a99b7d10163499c
Erika Eckfeld <erikazprincess@aol.com>
2019-10-23 15:40:26 -0500

    Added navigation to color picker application

--- Color Picker/Color Picker/AppDelegate.swift
+++ Color Picker/Color Picker/AppDelegate.swift
@@ -15,12 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-      
+        
         return true
     }
     
     func applicationWillResignActive(_ application: UIApplication) {
-         return true
+        
     }
     
     func applicationDidEnterBackground(_ application: UIApplication) {
--- Color Picker/Color Picker/ViewController.swift
+++ Color Picker/Color Picker/ViewController.swift
@@ -8,57 +8,59 @@
 
 import UIKit
 
-class ColorPickerViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource{
+class ViewController: UIViewController {
     
-    struct Color{
-        let colorString : String
-        let colorUI : UIColor
-    }
+    @IBOutlet weak var colorLabel: UILabel!
+    @IBOutlet weak var colorPicker: UIPickerView!
     
-
-    
-    @IBOutlet weak var picker: UIPickerView!
-    @IBOutlet weak var label: UILabel!
-    
-    
-    var colorsDataSource = [Color(colorString : "Red", colorUI : UIColor.red) , Color(colorString : "Orange", colorUI : UIColor.orange), Color(colorString : "Green", colorUI : UIColor.green) , Color(colorString : "Blue", colorUI : UIColor.blue) , Color(colorString : "Purple", colorUI : UIColor.purple)]
+    let colors = [
+        Color(name: "Red", color: .red),
+        Color(name: "Orange", color: .orange),
+        Color(name: "Yellow", color: .yellow),
+        Color(name: "Green", color: .green),
+        Color(name: "Blue", color: .blue),
+        Color(name: "Purple", color: .purple)
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.picker.dataSource = self;
-        self.picker.delegate = self;
-        
-        let initialIndex = 0
-        pickerView(picker, didSelectRow: initialIndex, inComponent: initialIndex)
-        
+        // Do any additional setup after loading the view, typically from a nib.
+        colorPicker.delegate = self
+        colorPicker.dataSource = self
+        let firstColor = colors.first
+        colorLabel.text = firstColor?.name
+        view.backgroundColor = firstColor?.color
     }
-    
-    
-    func pickerView(_: UIPickerView, didSelectRow row: Int, inComponent component: Int)
-    {
-        label.text = colorsDataSource[row].colorString
-        view.backgroundColor = colorsDataSource[row].colorUI
-        
-    }
-    
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-        
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return colorsDataSource.count
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return colorsDataSource[row].colorString
-    }
-    
-    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+}
+
+extension ViewController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        colorLabel.text = colors[row].name
+        view.backgroundColor = colors[row].color
+    }
+}
+
+extension ViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return colors.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return colors[row].name
+    }
+}
+
+struct Color {
+    let name: String
+    let color: UIColor
 }
